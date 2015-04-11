a9fff4131cedda13f797c7fa700a56b015d7c44c
Connor Glosser <glosser1@gmail.com>
2015-04-10 22:46:36 -0400

    Implement partitioning & interactions, alleviate bug with current magnitudes

--- FMM.py
+++ FMM.py
@@ -6,7 +6,7 @@ from itertools     import groupby
 NUM_ANGLE_QUADRATURE  = 64
 DELTA_THETA           = 2*np.pi/(NUM_ANGLE_QUADRATURE - 1)
 DISCRETE_ANGLES       = np.linspace(0, 2*np.pi, NUM_ANGLE_QUADRATURE)
-DISCRETE_KHAT_VECTORS = np.transpose([np.cos(DISCRETE_ANGLES), 
+DISCRETE_KHAT_VECTORS = np.transpose([np.cos(DISCRETE_ANGLES),
                                       np.sin(DISCRETE_ANGLES)])
 K_NORM = 1.0
 HARMONIC_MAX = 15
@@ -20,6 +20,7 @@ class Grid(object):
         self.grid_density  = len(self.sources)/self.grid_length**2
         self.boxes_per_row = int(np.ceil(len(self.sources)**0.25))
         self.box_length    = self.grid_length/self.boxes_per_row
+        self.boxes = self.__partition_points()
 
     def __box_id(self, location):
         """Convert an integral (x, y) box coordinate to its unique integer id
@@ -38,25 +39,31 @@ class Grid(object):
         source point
         """
         coords = np.array([i.location for i in self.sources])
-        return np.floor(coords/self.box_length).astype(int) 
+        return np.floor(coords/self.box_length).astype(int)
 
-    def partition_points(self):
+    def __partition_points(self):
         box_ids = [self.__box_id(np.floor(i.location/self.box_length))
                 for i in self.sources]
 
         boxes = [[] for _ in range(self.boxes_per_row**2)]
-    
+
         for box_num in range(self.boxes_per_row**2):
             corner = self.__box_coords(box_num)*self.box_length
-            boxes[box_num] = Box(corner, 
-                    [s for idx, s in enumerate(self.sources) 
+            boxes[box_num] = Box(corner,
+                    [s for idx, s in enumerate(self.sources)
                         if box_ids[idx] == box_num])
 
         return boxes
 
+    def compute_box_interaction(self, src_id, obs_id):
+        src = self.boxes[src_id]
+        obs = self.boxes[obs_id]
 
+        planewaves = (obs.incoming_rays *
+                translation_operator(src, obs) *
+                src.outgoing_rays)
 
-
+        return np.trapz(planewaves, dx = DELTA_THETA)/(2*np.pi)
 
 class Box(object):
     def __init__(self, location, sources):
@@ -74,15 +81,15 @@ class Box(object):
         convention for *source* points; requires a conjugation for
         *observation* points.
         """
-        delta_r = np.array([p - self.location for p in self.points])
+        delta_r = self.points - self.location
         norms = np.array([np.linalg.norm(p) for p in delta_r])
         point_angles = np.arctan2(delta_r[:, 1], delta_r[:, 0])
-        cos_arg = np.array([alpha - point_angles 
+        cos_arg = np.array([alpha - point_angles
             for alpha in DISCRETE_ANGLES])
         return np.exp(-1j*K_NORM*norms*np.cos(cos_arg))
 
     def __source_expansion(self):
-        """Accumulate all of the planewave expansions, weighted by each 
+        """Accumulate all of the planewave expansions, weighted by each
         point-source's current, for the box acting as a *source*.
         """
         return np.dot(self.__planewaves(), self.currents)
@@ -103,14 +110,14 @@ def translation_operator(box1, box2):
     angle   = np.arctan2(delta_r[1], delta_r[0])
 
     hankel_terms = (hankel2(idx, K_NORM*dist)*np.exp(-1j*idx*
-        (angle - DISCRETE_ANGLES - np.pi/2)) 
+        (angle - DISCRETE_ANGLES - np.pi/2))
         for idx in range(-HARMONIC_MAX, HARMONIC_MAX + 1))
 
     return np.sum(hankel_terms, axis=0)
 
 def construct_sources(num, box_dim = 1):
-    """Assemble a collection of random pointlike current sources within 
+    """Assemble a collection of random pointlike current sources within
     [0, box_dim] in both x and y.
     """
-    return [PointCurrent(current = np.random.rand(), 
+    return [PointCurrent(current = 1,
         location = np.random.rand(2)*box_dim) for _ in range(num)]
