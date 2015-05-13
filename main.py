2cc2f07c34a29eee192e0d26343e6ac10e0376ac
Connor Glosser <glosser1@gmail.com>
2015-05-13 13:21:02 +0200

    Proper timing & error estimates

--- main.py
+++ main.py
@@ -12,30 +12,30 @@ def timing(func):
     return get_results
 
 @timing
-def fmm_interactions(sources, interaction_fn):
-    sim_grid = FMM.Grid(sources)
-    return np.array([interaction_fn(b1,b2) 
-                     for b1 in sim_grid.boxes 
-                     for b2 in sim_grid.boxes if b1 is not b2])
+def box_interactions(grid, interaction_fn):
+    return [np.sum([interaction_fn(src_box, obs_box) for (src_id, src_box) 
+            in enumerate(grid.boxes) if not grid.nearby(src_id, obs_id)], 0)
+            for (obs_id, obs_box) in enumerate(grid.boxes)]
 
 def main():
-    for numParticles in 2**np.arange(5,12):
-        sources = FMM.construct_sources(numParticles, 5)
-
+    for numParticles in 2**np.arange(5,15):
+        print(numParticles)
+        sources = FMM.construct_sources(numParticles, 100)
         sim_grid = FMM.Grid(sources)
 
-        print(numParticles)
-        t1, naiive_result = naiive_interaction(sim_grid.boxes[-1], sim_grid.boxes[1])
-        t2, fmm_result    = (0, 0) #timing(FMM.box_interaction, sim_grid.boxes[-1], sim_grid.boxes[1])
+        t1, naiive_result = box_interactions(sim_grid, FMM.naiive_interaction)
+        t2, fmm_result    = box_interactions(sim_grid, FMM.fmm_interaction)
         
-        print(fmm_result, "\n", naiive_result)
-        delta = fmm_result - naiive_result
-        print("Numerical error in FMM: {}".format(
-            np.linalg.norm(delta)/np.linalg.norm(naiive_result)
-            )
-        )
-        print("Naiive timing: {}".format(t1))
-        print("FMM timing:    {}".format(t2))
+        naiive_evals = np.hstack(naiive_result)
+        fmm_evals    = np.hstack(fmm_result)
+
+        error = np.linalg.norm(fmm_evals - naiive_evals)/np.linalg.norm(naiive_evals)
+
+        print("Naiive timing:  {}".format(t1))
+        print("FMM timing:     {}".format(t2))
+        print("Num boxes:      {}".format(sim_grid.num_boxes))
+        print("Absolute error: {}".format(error))
+        print()
 
 if __name__ == "__main__":
     main()
